d7aa002062ed9de8eb2666835707bae5ec9b96e6
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-01-16 18:31:37 +0530

    /Akshay

---
+++ src/main/java/com/ats/tril/model/IndentStatusPendingReport.java
@@ -0,0 +1,100 @@
+package com.ats.tril.model;
+ 
+
+public class IndentStatusPendingReport {
+	 
+	private int sr; 
+	private int indDId; 
+	private String indMNo; 
+	private String indMDate; 
+	private int itemId; 
+	private String itemCode;  
+	private String indItemSchddt; 
+	private String remark; 
+	private int excessDays; 
+	private float indQty; 
+	private float rate;
+	private float balQty;
+	public int getSr() {
+		return sr;
+	}
+	public void setSr(int sr) {
+		this.sr = sr;
+	}
+	public int getIndDId() {
+		return indDId;
+	}
+	public void setIndDId(int indDId) {
+		this.indDId = indDId;
+	}
+	public String getIndMNo() {
+		return indMNo;
+	}
+	public void setIndMNo(String indMNo) {
+		this.indMNo = indMNo;
+	}
+	public String getIndMDate() {
+		return indMDate;
+	}
+	public void setIndMDate(String indMDate) {
+		this.indMDate = indMDate;
+	}
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+	public String getItemCode() {
+		return itemCode;
+	}
+	public void setItemCode(String itemCode) {
+		this.itemCode = itemCode;
+	}
+	public String getIndItemSchddt() {
+		return indItemSchddt;
+	}
+	public void setIndItemSchddt(String indItemSchddt) {
+		this.indItemSchddt = indItemSchddt;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public int getExcessDays() {
+		return excessDays;
+	}
+	public void setExcessDays(int excessDays) {
+		this.excessDays = excessDays;
+	}
+	public float getIndQty() {
+		return indQty;
+	}
+	public void setIndQty(float indQty) {
+		this.indQty = indQty;
+	}
+	public float getRate() {
+		return rate;
+	}
+	public void setRate(float rate) {
+		this.rate = rate;
+	}
+	
+	public float getBalQty() {
+		return balQty;
+	}
+	public void setBalQty(float balQty) {
+		this.balQty = balQty;
+	}
+	@Override
+	public String toString() {
+		return "IndentStatusPendingReport [sr=" + sr + ", indDId=" + indDId + ", indMNo=" + indMNo + ", indMDate="
+				+ indMDate + ", itemId=" + itemId + ", itemCode=" + itemCode + ", indItemSchddt=" + indItemSchddt
+				+ ", remark=" + remark + ", excessDays=" + excessDays + ", indQty=" + indQty + ", rate=" + rate
+				+ ", balQty=" + balQty + "]";
+	}
+
+	
+}
