8066f5b5cf077d5d4b74f10ed507250e5b95415c
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-08-28 10:25:14 +0530

    /Akshay

--- src/main/java/com/ats/tril/common/Constants.java
+++ src/main/java/com/ats/tril/common/Constants.java
@@ -7,16 +7,19 @@ public class Constants {
 	public static final String url="http://localhost:8093/";
 	//public static final String url="http://tomcat.aaryatechindia.in:2908/webapi/"; 
 	//public static final String url="http://132.148.143.124:8080/TrilWebApi/"; 
+	//public static final String url="http://198.12.156.31:8080/TrilWebApi/"; 
 	//public static final String ReportURL = "http://tomcat.aaryatechindia.in:2908/admin";
-	public static final String ReportURL = "http://localhost:8080/tril";
-	//public static final String ReportURL = "http://132.148.143.124:8080/triladmin";
+	//public static final String ReportURL = "http://localhost:8080/tril";
+	public static final String ReportURL = "http://198.12.156.31:8080/TrimbakRubberAdmin";
 	public static final int ItemImage = 1;  
 	//public static final String Item_Image_URL ="C:/pdf/";
 	//public static final String Item_Image_URL ="http://tomcat.aaryatechindia.in:2908/uploads/Item/";
-	public static final String Item_Image_URL = "http://132.148.143.124:8080/triluploads/";
+	public static final String Item_Image_URL = "http://198.12.156.31:8080/triluploads/";
 	//public static String REPORT_SAVE = "/home/aaryate1/tomcat.aaryatechindia.in/tomcat-8.0.18/webapps/admin/report.pdf";
 	//public static String REPORT_SAVE = "/opt/apache-tomcat-8.5.6/webapps/triladmin/report.pdf";
-	public static String REPORT_SAVE = "/home/lenovo/Documents/pdf/Report.pdf";
+	public static String REPORT_SAVE = "/opt/apache-tomcat-8.5.39/webapps/TrimbakRubberAdmin/report.pdf";
+	
+	//public static String REPORT_SAVE = "/home/lenovo/Documents/pdf/Report.pdf";
 	public static int delStatus = 1;
 	public static int subAct=0;
 	public static int mainAct=0;
